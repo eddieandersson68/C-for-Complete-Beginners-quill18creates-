02fb1a34a7356553004f3677e6b7a5e56a45c1e8
eddieandersson68 <eddieandersson68@users.noreply.github.com>
2017-02-08 13:58:12 +0100

    Nytt projekt från Sussis dator
    Nytt projekt från Sussis dator

---
+++ DodgeGameSolution/DodgeGame/DodgeGameMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DodgeGame
+{
+	class DodgeGameMain
+	{
+		static void Main ()
+		{	
+			// Create a new game.
+			Game game = new Game ();
+
+			//Run the game.
+			game.run();
+
+			// When we get here the game is over.
+
+			//Program end
+			// Do some clean-up
+
+			//Set cursor to bottom most row
+			Console.SetCursorPosition (0, Console.WindowHeight + 1);
+
+
+		}
+	}
+} 
