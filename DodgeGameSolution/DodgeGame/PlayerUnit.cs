2bde8b9c10e5604fd744d794ce6a9eabd153c4a5
eddieandersson68 <eddie.andersson68@gmail.com>
2017-02-22 14:17:01 +0100

    latest update
    moved { down one row in the Player unit class on switch statement

--- DodgeGameSolution/DodgeGame/PlayerUnit.cs
+++ DodgeGameSolution/DodgeGame/PlayerUnit.cs
@@ -22,28 +22,29 @@ namespace DodgeGame
 
 				ConsoleKeyInfo cki = Console.ReadKey (true);
 
-				switch (cki.Key) {
+				switch (cki.Key) 
+				{
 
-				case ConsoleKey.UpArrow:
-				case ConsoleKey.W:
-				case ConsoleKey.NumPad8:
-					Y = Y - 1;
-					break;
-				case ConsoleKey.DownArrow:
-				case ConsoleKey.S:
-				case ConsoleKey.NumPad2:
-					Y = Y + 1;
-					break;
-				case ConsoleKey.LeftArrow:
-				case ConsoleKey.A:
-				case ConsoleKey.NumPad4:
-					X = X - 1;
-					break;
-				case ConsoleKey.RightArrow:
-				case ConsoleKey.D:
-				case ConsoleKey.NumPad6:
-					X = X + 1;
-					break;
+					case ConsoleKey.UpArrow:
+					case ConsoleKey.W:
+					case ConsoleKey.NumPad8:
+						Y = Y - 1;
+						break;
+					case ConsoleKey.DownArrow:
+					case ConsoleKey.S:
+					case ConsoleKey.NumPad2:
+						Y = Y + 1;
+						break;
+					case ConsoleKey.LeftArrow:
+					case ConsoleKey.A:
+					case ConsoleKey.NumPad4:
+						X = X - 1;
+						break;
+					case ConsoleKey.RightArrow:
+					case ConsoleKey.D:
+					case ConsoleKey.NumPad6:
+						X = X + 1;
+						break;
 				}
 
 				// Check to see if user prexsed w OR W
